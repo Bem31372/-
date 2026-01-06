a1b495acbc0866ad28a4aa2f1b4d79f4d3c51ef4
Manuchet <MC683422050248>
2026-01-06 15:24:51 +0700

    bank

--- bank.py
+++ bank.py
@@ -1,3 +1,20 @@
 class BankAccount:
     def __inti__(self):
         self.__balance__ = 0
+
+    def deposit(self, amount):
+        if amount >0:
+            self.__balance__ += amount
+            print(f'ฝากสำเร็จ: {amount} บาท')
+        else:
+            print('ยอดเงินฝากต้อง')
+    
+    def withdraw(slef,amount):
+        if amount > 0 and amount <= self.__balance:
+            self.__balance -= amount
+            print(f"ถอนเงินไม่สำเร็จ: {amount} บาท")
+        else:
+            print(f"ตอนไม่สำเร็จ:(ยอดเงินไม่เพียงพอต่อการถอน {amount} บาท)")
+
+        def check_balance(self):
+            return self.__balance
